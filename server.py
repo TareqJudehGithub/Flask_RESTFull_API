c90f5b226bbdf66bd5bb174cb374d4adf1cb59c1
TareqJudehGithub <tareq.joudeh@gmail.com>
2021-12-26 02:55:13 +0200

    Implemented Favorites List feature where users can add and remove their favorites titles. Favorites List data and CRUD operation were built using SQL Alchemy, where each CRUD operation has it's own route and view design.

--- server.py
+++ server.py
@@ -1,53 +1,149 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, jsonify, redirect
 from markupsafe import escape
 import requests
 import os
 from datetime import datetime
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 # Flask secret key
 secret_key = os.urandom(12)
 # Instantiating 'app' variable from class Flask
 app = Flask(__name__)
 app.secret_key = secret_key
+
 # current year
 year = datetime.now().year
 
-# API response
-response = "http://www.omdbapi.com/?apikey=deec4c57&s=star%20wars&type=movie"
+
+# DB configuration
+# DB location and name
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+# Initialize the DB
+db = SQLAlchemy(app)
+
+# Init marshmallow
+ma = Marshmallow(app)
+
+# Create model class
+class Movies(db.Model):
+  id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+  title = db.Column(db.String(200), nullable=False, unique=True)
+  poster = db.Column(db.String(200), nullable=True)
+  
+ 
+  # Constructor
+  def __init__(self, title, poster):
+    self.title = title
+    self.poster = poster
+      
+
+# Movie schema
+class MovieSchema(ma.Schema):
+  class Meta:
+    fields = ("id", "title", "poster")
+
+
+# Init Schema
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+# API key and response
+API_KEY = os.environ.get("API_KEY")
+response = f"http://www.omdbapi.com/?apikey={API_KEY}&s=harry%20potter&type=movie"
 # Assigning API response to 'data' variable 
 data = requests.get(url=response)
 
 
-# TODO TASK 1. GET: 
 # Main page route - movies route
-@app.route("/")
-def homepage():  
+@app.route("/", methods=["GET"])
+def homepage(): 
+   
   # Check GET response for returned data
   movies = data.json()
+  
   if movies["Response"] == "True":
     return render_template(
       escape("index.html"),
       movies=movies
     )
+    
+  
+# Return favorites titles  
+@app.route("/fav")
+def fav_movies():
+  favorites = Movies.query.all()
+  favorites_list = movies_schema.dump(favorites)
+  
+  if len(favorites_list) == 0:
+    flash("you have no items yet in Favorites.")
+
+  # Show favorites  
+  return render_template(
+    escape("favorites.html"),
+    favorites_list=favorites_list
+  )   
+  
+ 
+# Add to favorites
+@app.route("/add_fav/<title>")
+def add_title(title):
+
+  # Instantiate a json object from the API response
+  movie =  requests.get(
+  url=f"http://www.omdbapi.com/?apikey={API_KEY}&t={title}"
+  ).json()
+  
+  # Declare title and poster(title image) from 
+  title_name = movie["Title"]
+  poster = movie["Poster"]
+    
+  fav_item = Movies(title_name, poster)
+  if Movies.query.filter_by(title=title_name).first():
+    flash(f"{title_name} is already in your favorites!")
   else:
-    flash("Requested data not found")
-    return render_template(
-      escape("error.html")
-    )
+    db.session.add(fav_item)
+    db.session.commit()
+  
+    flash(f"{title_name} has been added to your favorites!")
+    # return redirect("/")
+  
+  return render_template(
+  escape("movie-info.html"),
+  movie=movie
+  )
+  
+  
+# Remove title from Favorites
+@app.route("/remove_fav/<title_id>")
+def remove_title(title_id):
+  title = Movies.query.get(title_id)
+  db.session.delete(title)
+  db.session.commit()
+  
+  flash(f"{title.title} has been removed from Favorites.")
+  return redirect("/fav")
+
 
 # Movie details route 
-@app.route("/move_info/<title_ID>")
+@app.route("/movie_info/<title_ID>", methods=["GET", "POST"])
 def movie_info(title_ID):
   movie =  requests.get(
-    url=f"http://www.omdbapi.com/?apikey=deec4c57&t={title_ID}"
+    url=f"http://www.omdbapi.com/?apikey={API_KEY}&t={title_ID}"
     ).json()
-  return render_template(
-    escape("movie-info.html"),
-    movie=movie
-  )
-
-# TODO TASK 2. POST: 
+ 
+  if request.method == "GET":
+    return render_template(
+      escape("movie-info.html"),
+      movie=movie
+    )
+  
+    
 # Advanced Search route
 @app.route('/search-movies', methods=["GET", "POST"])
 def search_movies():
@@ -60,11 +156,11 @@ def search_movies():
     # Check if the user provided Year or Type data
     if year != "" or type != "":
         movie = requests.get(
-          url=f"http://www.omdbapi.com/?apikey=deec4c57&t={title}&y={year}&type={type}"
+          url=f"http://www.omdbapi.com/?apikey={API_KEY}&t={title}&y={year}&type={type}"
           ).json()
     else:
       movie = requests.get(
-        url=f"http://www.omdbapi.com/?apikey=deec4c57&t={title}"
+        url=f"http://www.omdbapi.com/?apikey={API_KEY}&t={title}"
       ).json()
 
     # Check if no data returned upon searching 
@@ -94,7 +190,7 @@ def search_movies():
 def search_nav():
     #
     search_mov = request.form["search_mov"] 
-    movies = requests.get(url=f"http://www.omdbapi.com/?apikey=deec4c57&s={search_mov}").json()
+    movies = requests.get(url=f"http://www.omdbapi.com/?apikey={API_KEY}&s={search_mov}").json()
 
     
     # Display message for not finding any data and redirect to Adv Search
@@ -120,7 +216,6 @@ def page_not_found(error):
     escape("error.html"),
     error=error,  
   ), 404
-
 
 
 if __name__ == "__main__":
